28df77b59e7fe520e9ebf834b137a23e02a7304d
sahar-alghamdi <saharalghamdimsc@gmail.com>
2020-12-26 16:20:13 +0300

    Show Artists and Search Artist is Done

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -258,16 +258,9 @@ def delete_venue(venue_id):
 @app.route('/artists')
 def artists():
   # TODO: replace with real data returned from querying the database
-  data=[{
-    "id": 4,
-    "name": "Guns N Petals",
-  }, {
-    "id": 5,
-    "name": "Matt Quevedo",
-  }, {
-    "id": 6,
-    "name": "The Wild Sax Band",
-  }]
+  
+  data = Artist.query.all()
+
   return render_template('pages/artists.html', artists=data)
 
 @app.route('/artists/search', methods=['POST'])
@@ -275,13 +268,28 @@ def search_artists():
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
   # search for "band" should return "The Wild Sax Band".
+  search_term = request.form.get('search_term')
+  artists = Artist.query.filter(Artist.name.ilike('%'+search_term+'%'))
+  
+  results = []
+  for artist in artists:
+    now = datetime.now()
+    shows = artist.Show
+    upcoming_shows = []
+    for show in shows:
+      if datetime.strptime(show.start_time, '%m/%d/%Y %H:%M:%S.%f') >= now:
+        upcoming_shows.append(show)
+
+    results.append({
+      "id": artist.id,
+      "name": artist.name,
+      "num_upcoming_shows": len(upcoming_shows)
+    })
+
+
   response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
+    "count": artists.count(),
+    "data": results
   }
   return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
